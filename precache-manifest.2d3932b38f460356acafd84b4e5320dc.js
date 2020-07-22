720f94e6b701c9bdc35c81ba0eb3b0e9573a1377
karuna14 <kkaushik@bt.iitr.ac.in>
2020-07-22 16:41:14 +0530

    Updates

---
+++ precache-manifest.2d3932b38f460356acafd84b4e5320dc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "066d401204c15933086295c485249aa3",
+    "url": "/ENTERTAIN-THE-CREW/index.html"
+  },
+  {
+    "revision": "250b7e06325f24ec309c",
+    "url": "/ENTERTAIN-THE-CREW/static/css/main.9cf74271.chunk.css"
+  },
+  {
+    "revision": "5016b7645c4e530053eb",
+    "url": "/ENTERTAIN-THE-CREW/static/js/2.b4d33df7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ENTERTAIN-THE-CREW/static/js/2.b4d33df7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "250b7e06325f24ec309c",
+    "url": "/ENTERTAIN-THE-CREW/static/js/main.23ec0b69.chunk.js"
+  },
+  {
+    "revision": "0004328dce71b44d1c5e",
+    "url": "/ENTERTAIN-THE-CREW/static/js/runtime-main.c059a10f.js"
+  },
+  {
+    "revision": "855fb65cca6fc107ada452b54a06b480",
+    "url": "/ENTERTAIN-THE-CREW/static/media/human.855fb65c.svg"
+  },
+  {
+    "revision": "04f4be75055e03ddf97b8532c14b039e",
+    "url": "/ENTERTAIN-THE-CREW/static/media/robot.04f4be75.svg"
+  }
+]);
